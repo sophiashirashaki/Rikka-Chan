e68fa7b693d36a8ab673573549e589b531187c0f
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-10-30 14:31:25 +0800

    Update logomaker.py

--- YoneRobot/modules/logomaker.py
+++ YoneRobot/modules/logomaker.py
@@ -36,7 +36,7 @@ async def lego(event):
     draw.text((x, y), text, font=font, fill="black", stroke_width=25, stroke_fill="yellow")
     fname2 = "LogoByYone.png"
     img.save(fname2, "png")
-    await tbot.send_file(event.chat_id, fname2, caption="Made By YoneRobot")
+    await tbot.send_file(event.chat_id, fname2, caption="Made By Rikka-Chan")
     if os.path.exists(fname2):
             os.remove(fname2)
  except Exception as e:
@@ -57,7 +57,7 @@ async def lego(event):
        return
     else:
        pass
- await event.reply('Creating your logo...wait!')
+ await event.reply('Riika creating your logo...wait!')
  try:
     text = event.pattern_match.group(1)
     img = Image.open('./YoneRobot/resources/blackbg.jpg')
@@ -88,7 +88,7 @@ file_helpo = file_help.replace("_", " ")
 
 
 __help__ = """
- ❍ /logo text :  Create your logo with your name
+ ❍ /logo text :  Make a logo from your request
 
  """
 __mod_name__ = "Logo"
