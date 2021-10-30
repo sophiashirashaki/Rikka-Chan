2c814e7b94585aa686bb3c399b7dfd2a20aac7e6
Rikka-Chan <93366308+sophiashirashaki@users.noreply.github.com>
2021-10-30 14:36:42 +0800

    Update backups.py

--- YoneRobot/modules/backups.py
+++ YoneRobot/modules/backups.py
@@ -341,8 +341,8 @@ def export_data(update, context):
         pass
     context.bot.sendDocument(
         current_chat_id,
-        document=open("YoneRobot{}.backup".format(chat_id), "rb"),
-        caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `YoneRobot-Backup` was specially made for notes 📚.".format(
+        document=open("RikkaBotz}.backup".format(chat_id), "rb"),
+        caption="💾*Successfully Exported backup:*\nChat: `{}`\nChat ID: `{}`\nOn: `{}`\n\nNote: This `Rikka-Chan_Backup` was specially made for notes 📚.".format(
             chat.title, chat_id, tgl
         ),
         timeout=360,
